f09dbb23c8d998473434b8d1f7fc72557bc864b0
devusmonjon <usmonjonhasanov777@gmail.com>
2025-02-02 19:53:35 +0500

    fix

--- src/database/user.model.ts
+++ src/database/user.model.ts
@@ -1,4 +1,4 @@
-import mongoose from "mongoose";
+import mongoose from "mongoose" ;
 
 const userSchema = new mongoose.Schema(
   {
@@ -12,7 +12,7 @@ const userSchema = new mongoose.Schema(
       required: true,
     },
   },
-  { timestamps: true }
+  { timestamps: true}
 );
 
 const User = mongoose.models.Admins || mongoose.model("Admins", userSchema);
