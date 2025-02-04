758a3101d69c44425bfd6ad42b906b6e65a5a91e
devusmonjon <usmonjonhasanov777@gmail.com>
2025-02-05 00:00:01 +0500

    fix

--- src/app/api/uploads/[filename]/route.ts
+++ src/app/api/uploads/[filename]/route.ts
@@ -2,14 +2,18 @@ import fs from "fs";
 import path from "path";
 import { NextResponse } from "next/server";
 
-export async function GET(req: Request, { params }: { params: { filename: string } }) {
-  const { filename } = await params;
-
-  if (!filename) return NextResponse.json({ error: "File not found" }, { status: 404 });
-
-  const filePath = path.join(process.cwd(), "public/uploads", filename);
-
+export async function GET(
+  req: Request,
+  { params }: { params: { filename: string } }
+) {
   try {
+    const { filename } = await params;
+
+    if (!filename)
+      return NextResponse.json({ error: "File not found" }, { status: 404 });
+
+    const filePath = path.join(process.cwd(), "public/uploads", filename);
+
     const fileBuffer = fs.readFileSync(filePath);
     return new Response(fileBuffer, {
       headers: { "Content-Type": "image/png" }, // MIME turiga qarab o‘zgartiring
