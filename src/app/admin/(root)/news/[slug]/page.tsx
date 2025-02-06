8d27b5080c845e24d25d57d3ddb7b01582ab790c
devusmonjon <usmonjonhasanov777@gmail.com>
2025-02-06 10:10:46 +0500

    fix

--- src/app/admin/(root)/news/[slug]/page.tsx
+++ src/app/admin/(root)/news/[slug]/page.tsx
@@ -1,19 +1,15 @@
-import { NewsItem } from "@/types/news";
 import { getNews } from "../server-action"
 import NewEditPageInner from "./page-view";
 
 export const dynamic = "force-dynamic"; // Sahifani har doim dynamic qilish
 export const revalidate = 0; // Sahifani har doim yangilash
 
-export async function generateStaticParams() {
-    const newsList = await getNews(); // Barcha yangiliklarni olish
-    return JSON.parse(newsList).map((news: NewsItem) => ({ slug: news.slug }));
+const NewsEditPage = async ({ params }: { params: {slug: string} }) => {
+    // @next-codemod-ignore
+    const news = await getNews(params.slug);
+  return (
+    <NewEditPageInner news={JSON.parse(news)}  />
+  )
 }
 
-const NewsEditPage = async ({ params }: { params: { slug: string } }) => {
-    const news = await getNews(params.slug); // Xatolik yo'q
-
-    return <NewEditPageInner news={JSON.parse(news)} />;
-};
-
-export default NewsEditPage;
+export default NewsEditPage
