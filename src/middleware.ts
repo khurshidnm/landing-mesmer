9dfd90a76798935655ce365dd8b734ae59982e6e
Ulugbek <mirzarakhmanoff@gmail.com>
2025-02-11 15:40:40 +0500

    done

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,9 +1,8 @@
-import createMiddleware from 'next-intl/middleware';
-import {routing} from './i18n/routing';
- 
+import createMiddleware from "next-intl/middleware";
+import { routing } from "./i18n/routing";
+
 export default createMiddleware(routing);
- 
+
 export const config = {
-  // Match only internationalized pathnames
-  matcher: ['/', '/(uz|en|ru)/:path*']
+  matcher: ["/", "/(uz|en|ru)/:path*"],
 };
