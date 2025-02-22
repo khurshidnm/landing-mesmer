7f7a4ae4e636ed3aa4bbb80389e5c247d5b51af8
Khurshid Normurodov <44772355+khurshidnm@users.noreply.github.com>
2025-02-22 15:41:03 +0500

    Update layout.jsx

--- src/app/[locale]/layout.jsx
+++ src/app/[locale]/layout.jsx
@@ -2,7 +2,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "../globals.css";
 import { Provider } from "./provider";
 import { Toaster } from "@/components/ui/toaster";
-import Script from "next/script"; // Step 1:
+import Script from "next/script"; // Import the Script component
 
 // next-intl
 import { NextIntlClientProvider } from "next-intl";
@@ -127,9 +127,8 @@ export default async function LocaleLayout({ children, params }) {
 
   return (
     <html lang={locale} suppressHydrationWarning={true}>
-
       <head>
-        {/* Step 2: */}
+        {/* Google Analytics Script */}
         <Script
           strategy="afterInteractive"
           src="https://www.googletagmanager.com/gtag/js?id=G-5NRZXYKKXW"
@@ -146,11 +145,42 @@ export default async function LocaleLayout({ children, params }) {
             `,
           }}
         />
+
+        {/* Yandex Metrika Script */}
+        <Script
+          id="yandex-metrika"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+              m[i].l=1*new Date();
+              for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
+              k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+              (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+
+              ym(100012016, "init", {
+                   clickmap:true,
+                   trackLinks:true,
+                   accurateTrackBounce:true
+              });
+            `,
+          }}
+        />
       </head>
-      
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        {/* Yandex Metrika Noscript Fallback */}
+        <noscript>
+          <div>
+            <img
+              src="https://mc.yandex.ru/watch/100012016"
+              style={{ position: "absolute", left: "-9999px" }}
+              alt=""
+            />
+          </div>
+        </noscript>
+
         <NextIntlClientProvider messages={messages}>
           <Toaster />
           <Provider>{children}</Provider>
