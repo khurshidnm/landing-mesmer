349e959ad10faf2f897683324ac8f74476447b95
Khurshid Normurodov <44772355+khurshidnm@users.noreply.github.com>
2025-02-22 16:29:01 +0500

    Update page.tsx

--- src/app/[locale]/(root)/contact/page.tsx
+++ src/app/[locale]/(root)/contact/page.tsx
@@ -139,7 +139,7 @@ const Contact = () => {
                 </div>
               </div>
 
-              <div className="flex items-center gap-8">
+              <div>
                 {socialLinks.map((social) => (
                   <Link
                     key={social.label}
